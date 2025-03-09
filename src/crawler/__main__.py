b104d56bb69b259d313e5a8fd53d9d772e14966c
saltchicken <john.eicher89@gmail.com>
2025-03-09 11:50:13 -0700

    Add argparse

--- src/crawler/__main__.py
+++ src/crawler/__main__.py
@@ -1,14 +1,19 @@
+import argparse
 from .scraper import scrape_website
 from .llm.api import query_ollama
 
 
 def main():
-    url = "https://en.wikipedia.org/wiki/Java"
-    page_content = scrape_website(url)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="The URL to scrape")
+    args = parser.parse_args()
+
+    page_content = scrape_website(args.url)
     system_message = "Summarize the following text"
     for paragraph in page_content.paragraphs:
-        response = query_ollama("test", paragraph, system_message, verbose=True)
-        print(response)
+        print(paragraph)
+        # response = query_ollama("test", paragraph, system_message, verbose=True)
+        # print(response)
 
 
 if __name__ == "__main__":
